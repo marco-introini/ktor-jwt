4d9931db74c6d72fc94e04ffc8cfb3959ae36ec9
Marco "Mint" Introini <mint.dev@pm.me>
2024-05-25 21:29:20 +0200

    Generated from KTOR generator

---
+++ src/main/kotlin/me/mintdev/Application.kt
@@ -0,0 +1,14 @@
+package me.mintdev
+
+import io.ktor.server.application.*
+import me.mintdev.plugins.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureSecurity()
+    configureRouting()
+}
