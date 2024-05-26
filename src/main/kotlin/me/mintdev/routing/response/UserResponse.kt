0f8901ba00ce1835c70c74eb559eb28a27dbe898
Marco "Mint" Introini <mint.dev@pm.me>
2024-05-26 08:56:58 +0200

    Add UUID serialization utility and usage
    A UUID serializer utility is introduced in this commit to handle the serialization and deserialization processes for UUID objects. This utility has been employed in the 'UserResponse' data class to handle the 'id' field serialization.

--- src/main/kotlin/me/mintdev/routing/response/UserResponse.kt
+++ src/main/kotlin/me/mintdev/routing/response/UserResponse.kt
@@ -1,10 +1,12 @@
 package me.mintdev.routing.response
 
 import kotlinx.serialization.Serializable
+import me.mintdev.utils.UUIDSerializer
 import java.util.UUID
 
 @Serializable
 data class UserResponse(
+    @Serializable(with = UUIDSerializer::class)
     val id: UUID,
     val username: String,
 )
