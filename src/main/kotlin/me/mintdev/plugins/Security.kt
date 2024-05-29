cf73da5b9f8647763759390e23256711def737c7
Marco "Mint" Introini <mint.dev@pm.me>
2024-05-29 11:45:36 +0200

    Cleanup

--- src/main/kotlin/me/mintdev/plugins/Security.kt
+++ src/main/kotlin/me/mintdev/plugins/Security.kt
@@ -1,11 +1,8 @@
 package me.mintdev.plugins
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
-import io.ktor.server.response.*
 import me.mintdev.service.JwtService
 
 fun Application.configureSecurity(jwtService: JwtService) {
